6e584ba2cb78a9e9c1935d6b9b1ddc64ffef98bb
Scott Key <scottdkey@gmail.com>
2021-05-05 13:05:45 -0600

    cleanup

--- src/main/kotlin/rsk/tailrec.kt
+++ src/main/kotlin/rsk/tailrec.kt
@@ -1,4 +1,10 @@
 package rsk
 
-class tailrec {
+import java.math.BigInteger
+
+fun main(args: Array<String>){
+    println(fib(10, BigInteger("1"), BigInteger("0")))
+}
+tailrec fun fib(n: Int, a: BigInteger, b: BigInteger): BigInteger {
+    return if(n == 0) b else fib(n-1, a+b, a)
 }
